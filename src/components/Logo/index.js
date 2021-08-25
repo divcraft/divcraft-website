1d0b05b4debd40db5c328e9f442bdde4e5bfaf6c
divcraft <t.ogonowski@wp.pl>
2021-08-25 19:47:45 +0200

    add image to logo component

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,7 +1,15 @@
+import { Link } from 'gatsby';
 import React from 'react';
+import { LogoContainer, LogoImg } from './style';
 
 const Logo = () => {
-  return <div>logo</div>;
+  return (
+    <LogoContainer>
+      <Link to="/">
+        <LogoImg src="/logo.svg" alt="logo" />
+      </Link>
+    </LogoContainer>
+  );
 };
 
 export default Logo;
---
+++ src/components/Logo/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const LogoContainer = styled.div`
+  height: 25px;
+`;
+
+export const LogoImg = styled.img`
+  height: 25px;
+`;
