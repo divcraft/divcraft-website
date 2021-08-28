e0ac2d7d045bc34db8d45b8a343a2ca5939d2142
divcraft <t.ogonowski@wp.pl>
2021-08-28 21:18:27 +0200

    improve titles styles in banner

--- src/templates/Banner/style.js
+++ src/templates/Banner/style.js
@@ -23,7 +23,7 @@ export const Content = styled.div`
   @media (min-width: 768px) {
     align-items: flex-start;
     justify-content: center;
-    width: 43%;
+    width: 42%;
     height: 40vw;
     transform: translateY(-10px);
   }
@@ -31,14 +31,30 @@ export const Content = styled.div`
 
 export const Title = styled.h1`
   color: white;
+  line-height: 1.25;
+  padding-bottom: 20px;
+  margin-bottom: 10px;
   font-size: calc(24px + 40 * ((100vw - 320px) / 680));
-  border-bottom: solid 1px white;
+  position: relative;
+  &::before {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    content: '';
+    height: 1px;
+    width: 100%;
+    background-color: white;
+  }
   @media (min-width: 768px) {
     font-size: calc(20px + 15 * ((100vw - 320px) / 680));
+    &::before {
+      width: 320px;
+    }
   }
 `;
 
 export const SubTitle = styled.h2`
+  margin-bottom: 20px;
   color: ${({ theme: { colors } }) => colors.yellow};
   font-size: calc(16px + 24 * ((100vw - 320px) / 680));
   @media (min-width: 768px) {
