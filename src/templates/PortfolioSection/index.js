7eccf7cb7fa6cf3ef5882bfb29387be5471b996a
divcraft <t.ogonowski@wp.pl>
2021-09-05 11:13:15 +0200

    add api data to portfolio section component

--- src/templates/PortfolioSection/index.js
+++ src/templates/PortfolioSection/index.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import { Wrapper, TitleHeader } from 'components';
+import portfolioContent from 'api/portfolioData';
 
 const PortfolioSection = () => {
+  const {
+    allFile: { nodes },
+  } = useStaticQuery(
+    graphql`
+      query PortfolioImageQuery {
+        allFile(filter: { relativePath: { regex: "/portfolio/" } }) {
+          nodes {
+            childrenImageSharp {
+              fluid {
+                src
+                srcSet
+              }
+            }
+          }
+        }
+      }
+    `
+  );
+  const reorderedNodes = [nodes[2], nodes[1], nodes[3], nodes[0]];
+  const portfolioData = portfolioContent.map((item, index) => {
+    return {
+      ...item,
+      src: reorderedNodes[index].childrenImageSharp[0].fluid.src,
+      srcSet: reorderedNodes[index].childrenImageSharp[0].fluid.srcSet,
+    };
+  });
+  const portfolio = portfolioData.map(item => (
+    <div key={item.url}>
+      <a href={item.url} target="_blank" rel="noreferrer">
+        {item.name}
+      </a>
+      <img src={item.src} alt={item.name} srcSet={item.srcSet} />
+      <p>{item.description}</p>
+      <a href={item.url} target="_blank" rel="noreferrer">
+        Zobacz stronę
+      </a>
+    </div>
+  ));
   return (
     <section>
       <Wrapper>
         <TitleHeader color="blue">Wykonane przez nas projekty</TitleHeader>
+        {portfolio}
       </Wrapper>
     </section>
   );
---
+++ src/templates/PortfolioSection/style.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const ArticleContainer = styled.article`
+  background-color: ${({ theme: { colors } }) => colors.black};
+`;
