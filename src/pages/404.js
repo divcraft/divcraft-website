8cc399cc9590fb1e80b659e159c5e9969a0a91f0
divcraft <t.ogonowski@wp.pl>
2021-09-11 19:07:55 +0200

    create and style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Layout from 'layouts';
+import { TitleHeader, Wrapper, Button } from 'components';
 
 const NotFoundPage = () => {
   return (
     <Layout>
-      <div>404 not found</div>
+      <section>
+        <Wrapper>
+          <TitleHeader color="blue">Strona nie istnieje</TitleHeader>
+          <p>
+            Link, który próbujesz uruchomić nie jest powiązany z żadną z naszych
+            stron. Przejdź z powrotem na stronę główną i zacznić przeglądać na
+            nowo ;)
+          </p>
+          <Button to="/" pattern="gatsbyLink">
+            Strona główna
+          </Button>
+        </Wrapper>
+      </section>
     </Layout>
   );
 };
